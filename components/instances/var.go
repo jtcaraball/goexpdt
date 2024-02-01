410d48318b9170f2a3350ce1175f0c63ccc1e07c
Jose Caraball <jtcaraball@Joses-MacBook-Air.local>
2024-02-01 13:12:36 -0500

    Add var instance struct and methods.

---
+++ components/instances/var.go
@@ -0,0 +1,63 @@
+package inst
+
+import (
+	"stratifoiled/cnf"
+	"stratifoiled/components"
+)
+
+// =========================== //
+//           STRUCTS           //
+// =========================== //
+
+type Var string
+
+// =========================== //
+//           METHODS           //
+// =========================== //
+
+func (v Var) addVariables(ctx *components.Context) {
+	for i := 0; i < ctx.Dimension; i++ {
+		for _, s := range components.Symbols {
+			ctx.AddVar(string(v), i, s)
+		}
+	}
+}
+
+func (v Var) Encode(ctx *components.Context) *cnf.CNF {
+	// If variable already exists then we return an always true CNF.
+	if ctx.VarExists(string(v), 0, components.BOT) {
+		return &cnf.CNF{}
+	}
+	v.addVariables(ctx)
+	nCNF := &cnf.CNF{}
+	// Add consistency clauses
+	// Every feature must have at least one value
+	reqAllFeats := [][]int{}
+	for i := 0; i < ctx.Dimension; i++ {
+		clause := []int{}
+		for _, s := range components.Symbols {
+			clause = append(clause, ctx.VarVal(string(v), i, s))
+		}
+		reqAllFeats = append(reqAllFeats, clause)
+	}
+	nCNF.ExtendConsistency(reqAllFeats)
+	// Every feature must have one and only one value
+	for i := 0; i < ctx.Dimension; i++ {
+		reqOnePerFeat := [][]int{
+			{
+				-ctx.VarVal(string(v), i, components.ZERO),
+				-ctx.VarVal(string(v), i, components.ONE),
+			},
+			{
+				-ctx.VarVal(string(v), i, components.ZERO),
+				-ctx.VarVal(string(v), i, components.BOT),
+			},
+			{
+				-ctx.VarVal(string(v), i, components.ONE),
+				-ctx.VarVal(string(v), i, components.BOT),
+			},
+		}
+		nCNF.ExtendConsistency(reqOnePerFeat)
+	}
+	return nCNF
+}
---
+++ components/instances/var_test.go
@@ -0,0 +1,53 @@
+package inst
+
+import (
+	"slices"
+	"stratifoiled/components"
+	"testing"
+)
+
+// =========================== //
+//           HELPERS           //
+// =========================== //
+
+func clausesEq(c1, c2 [][]int) bool {
+	return slices.EqualFunc[[][]int](
+		c1,
+		c2,
+		func (l1, l2 []int) bool {
+			return slices.Equal[[]int](l1, l2)
+		},
+	)
+}
+
+// =========================== //
+//            TESTS            //
+// =========================== //
+
+func TestVar_Encode(t *testing.T) {
+	var Variable Var = "testVar"
+	ctx := components.NewContext(1, nil)
+	encCNF := Variable.Encode(ctx)
+	sClauses, cClauses := encCNF.Clauses()
+	expSClauses := [][]int{}
+	expCClauses := [][]int{
+		{1, 2, 3},
+		{-1, -2},
+		{-1, -3},
+		{-2, -3},
+	}
+	if !clausesEq(sClauses, expSClauses) {
+		t.Errorf(
+			"Clauses not equal. Expected %d but got %d",
+			expSClauses,
+			sClauses,
+		)
+	}
+	if !clausesEq(cClauses, expCClauses) {
+		t.Errorf(
+			"Clauses not equal. Expected %d but got %d",
+			expCClauses,
+			cClauses,
+		)
+	}
+}

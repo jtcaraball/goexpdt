3444ecad46f42b19170c7cf9cb1bb090e1f8413f
Jose Caraball <jtcaraball@Joses-MacBook-Air.local>
2024-02-07 10:44:23 -0500

    Add allComp.Const and testing.

---
+++ components/circuits/allcomp/const.go
@@ -0,0 +1,123 @@
+package allcomp
+
+import (
+	"fmt"
+	"errors"
+	"stratifoiled/cnf"
+	"stratifoiled/trees"
+	"stratifoiled/components"
+	"stratifoiled/components/instances"
+)
+
+// =========================== //
+//           STRUCTS           //
+// =========================== //
+
+type acConst struct {
+	constInst instances.Const
+	leafValue bool
+}
+
+// =========================== //
+//           METHODS           //
+// =========================== //
+
+// Return varVar lel.
+func Const(constInst instances.Const, leafValue bool) *acConst {
+	return &acConst{constInst: constInst, leafValue: leafValue}
+}
+
+// Return CNF encoding of component.
+func (ac *acConst) Encoding(ctx *components.Context) (*cnf.CNF, error) {
+	if err := ac.validateInstances(ctx); err != nil {
+		return nil, err
+	}
+	if ctx.Tree == nil || ctx.Tree.Root == nil {
+		return nil, errors.New("Tree or it's root is nil")
+	}
+	var node *trees.Node
+	var nStack  = []*trees.Node{ctx.Tree.Root}
+	for len(nStack) > 0 {
+		node, nStack = nStack[len(nStack) - 1], nStack[:len(nStack) - 1]
+		if node.IsLeaf() {
+			if node.Value != ac.leafValue {
+				return cnf.CNFFromClauses([][]int{{}}), nil
+			}
+			continue
+		}
+		if ac.constInst[node.Feat] == instances.BOT {
+			nStack = append(nStack, node.LChild, node.RChild)
+			continue
+		}
+		if ac.constInst[node.Feat] == instances.ONE {
+			nStack = append(nStack, node.RChild)
+			continue
+		}
+		if ac.constInst[node.Feat] == instances.ZERO {
+			nStack = append(nStack, node.LChild)
+			continue
+		}
+	}
+	return &cnf.CNF{}, nil
+}
+
+// Return pointer to simplified equivalent component which might be itself.
+// This method may change the state of the caller.
+func (ac *acConst) Simplified(
+	ctx *components.Context,
+) (components.Component, error) {
+	if err := ac.validateInstances(ctx); err != nil {
+		return nil, err
+	}
+	if ctx.Tree == nil || ctx.Tree.Root == nil {
+		return nil, errors.New("Tree or it's root is nil")
+	}
+	var node *trees.Node
+	var nStack  = []*trees.Node{ctx.Tree.Root}
+	for len(nStack) > 0 {
+		node, nStack = nStack[len(nStack) - 1], nStack[:len(nStack) - 1]
+		if node.IsLeaf() {
+			if node.Value != ac.leafValue {
+				return components.NewTrivial(false), nil
+			}
+			continue
+		}
+		if ac.constInst[node.Feat] == instances.BOT {
+			nStack = append(nStack, node.LChild, node.RChild)
+			continue
+		}
+		if ac.constInst[node.Feat] == instances.ONE {
+			nStack = append(nStack, node.RChild)
+			continue
+		}
+		if ac.constInst[node.Feat] == instances.ZERO {
+			nStack = append(nStack, node.LChild)
+			continue
+		}
+	}
+	return components.NewTrivial(true), nil
+}
+
+// Return slice of pointers to component's children.
+func (ac *acConst) GetChildren() []components.Component {
+	return []components.Component{}
+}
+
+// yes is true if struct is trivial and value represents its truthiness.
+func (ac *acConst) IsTrivial() (yes bool, value bool) {
+	return false, false
+}
+
+// Return error if constInst value is invalid.
+func (ac *acConst) validateInstances(ctx *components.Context) error {
+	if len(ac.constInst) != ctx.Dimension {
+		return errors.New(
+			fmt.Sprintf(
+				"lel.constVar -> constant: wrong dim %d (%d feats in context)",
+				len(ac.constInst),
+				ctx.Dimension,
+			),
+		)
+	}
+	return nil
+}
--- components/circuits/allcomp/const_test.go
+++ components/circuits/allcomp/const_test.go
@@ -3,21 +3,19 @@ package allcomp
 import (
 	"fmt"
 	"stratifoiled/components"
-	"stratifoiled/components/circuits/subsumption"
 	"stratifoiled/components/instances"
-	"stratifoiled/components/operators"
 	"stratifoiled/sfdtest"
 	"stratifoiled/trees"
 	"testing"
 )
 
-const varSUFIX = "allComp.var"
+const constSUFIX = "allComp.const"
 
 // =========================== //
 //           HELPERS           //
 // =========================== //
 
-func runAllCompVar(
+func runAllCompConst(
 	t *testing.T,
 	id, expCode int,
 	c instances.Const,
@@ -27,19 +25,9 @@ func runAllCompVar(
 ) {
 	var err error
 	var formula components.Component
-	x := instances.NewVar("x")
 	context := components.NewContext(DIM, tree)
-	formula = operators.WithVar(
-		x,
-		operators.And(
-			operators.And(
-				subsumption.ConstVar(c, x),
-				subsumption.VarConst(x, c),
-			),
-			Var(x, leafValue),
-		),
-	)
-	filePath := sfdtest.CNFName(compVarSufix(leafValue), id, simplify)
+	formula = Const(c, leafValue)
+	filePath := sfdtest.CNFName(compConstSufix(leafValue), id, simplify)
 	if simplify {
 		formula, err = formula.Simplified(context)
 		if err != nil {
@@ -59,55 +47,75 @@ func runAllCompVar(
 	sfdtest.RunFormulaTest(t, id, expCode, filePath)
 }
 
-func compVarSufix(val bool) string {
-	return varSUFIX + fmt.Sprintf("%t", true)
+func compConstSufix(val bool) string {
+	return constSUFIX + fmt.Sprintf("%t", true)
 }
 
 // =========================== //
 //            TESTS            //
 // =========================== //
 
-func TestVar_Encoding_AllPos(t *testing.T) {
-	sfdtest.AddCleanup(t, compVarSufix(true), false)
+func TestConst_Encoding_AllPos(t *testing.T) {
+	sfdtest.AddCleanup(t, compConstSufix(true), false)
 	tree := genTree()
 	for i, tc := range allPosTests {
 		t.Run(tc.name, func(t *testing.T) {
-			runAllCompVar(t, i, tc.expCode, tc.val, tree, true, false)
+			runAllCompConst(t, i, tc.expCode, tc.val, tree, true, false)
 		})
 	}
 }
 
-func TestVar_Encoding_AllNeg(t *testing.T) {
-	sfdtest.AddCleanup(t, compVarSufix(false), false)
+func TestConst_Encoding_AllNeg(t *testing.T) {
+	sfdtest.AddCleanup(t, compConstSufix(false), false)
 	tree := genTree()
 	for i, tc := range allNegTests {
 		t.Run(tc.name, func(t *testing.T) {
-			runAllCompVar(t, i, tc.expCode, tc.val, tree, false, false)
+			runAllCompConst(t, i, tc.expCode, tc.val, tree, false, false)
 		})
 	}
 }
 
-func TestVar_Simplified_AllPos(t *testing.T) {
-	sfdtest.AddCleanup(t, compVarSufix(true), false)
+func TestConst_Encoding_WrongDim(t *testing.T) {
+	x := instances.Const{instances.BOT, instances.BOT, instances.BOT}
+	formula := Const(x, true)
+	context := components.NewContext(4, &trees.Tree{Root: &trees.Node{}})
+	_, err := formula.Encoding(context)
+	if err == nil {
+		t.Error("Error not cached. Expected constant wrong dimension error")
+	}
+}
+
+func TestConst_Simplified_AllPos(t *testing.T) {
+	sfdtest.AddCleanup(t, compConstSufix(true), false)
 	tree := genTree()
 	for i, tc := range allPosTests {
 		t.Run(tc.name, func(t *testing.T) {
-			runAllCompVar(t, i, tc.expCode, tc.val, tree, true, true)
+			runAllCompConst(t, i, tc.expCode, tc.val, tree, true, true)
 		})
 	}
 }
 
-func TestVar_Simplified_AllNeg(t *testing.T) {
-	sfdtest.AddCleanup(t, compVarSufix(false), false)
+func TestConst_Simplified_AllNeg(t *testing.T) {
+	sfdtest.AddCleanup(t, compConstSufix(false), false)
 	tree := genTree()
 	for i, tc := range allNegTests {
 		t.Run(tc.name, func(t *testing.T) {
-			runAllCompVar(t, i, tc.expCode, tc.val, tree, false, true)
+			runAllCompConst(t, i, tc.expCode, tc.val, tree, false, true)
 		})
 	}
 }
 
-func TestVar_GetChildren(t *testing.T) {
+func TestConstConst_Simplified_WrongDim(t *testing.T) {
+	x := instances.Const{instances.BOT, instances.BOT, instances.BOT}
+	formula := Const(x, true)
+	context := components.NewContext(4, &trees.Tree{Root: &trees.Node{}})
+	_, err := formula.Simplified(context)
+	if err == nil {
+		t.Error("Error not cached. Expected constant wrong dimension error")
+	}
+}
+
+func TestConst_GetChildren(t *testing.T) {
 	x := instances.NewVar("x")
 	formula := Var(x, true)
 	children := formula.GetChildren()
@@ -120,7 +128,7 @@ func TestVar_GetChildren(t *testing.T) {
 	}
 }
 
-func TestVar_IsTrivial(t *testing.T) {
+func TestConst_IsTrivial(t *testing.T) {
 	x := instances.NewVar("x")
 	formula := Var(x, true)
 	isTrivial, _ := formula.IsTrivial()

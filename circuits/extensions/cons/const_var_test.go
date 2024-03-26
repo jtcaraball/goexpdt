2f58e212bcca9f2581feae6361773d5247aa3bed
jo <jo@pop-os.localdomain>
2024-03-26 15:05:13 -0300

    feat: add cons ConstVar circuit.

---
+++ circuits/extensions/cons/const_var_test.go
@@ -0,0 +1,154 @@
+package cons
+
+import (
+	"goexpdt/base"
+	"goexpdt/circuits/predicates/subsumption"
+	"goexpdt/operators"
+	"goexpdt/circuits/internal/test"
+	"testing"
+)
+
+const constVarSUFIX = "cons.constvar"
+const guardedConstVarSUFIX = "cons.Gconstvar"
+
+// =========================== //
+//           HELPERS           //
+// =========================== //
+
+func runConsConstVar(
+	t *testing.T,
+	id, expCode int,
+	c1, c2 base.Const,
+	simplify bool,
+) {
+	x := base.NewVar("x")
+	context := base.NewContext(DIM, nil)
+	formula := operators.WithVar(
+		x,
+		operators.And(
+			operators.And(
+				subsumption.VarConst(x, c2),
+				subsumption.ConstVar(c2, x),
+			),
+			ConstVar(c1, x),
+		),
+	)
+	filePath := test.CNFName(constVarSUFIX, id, simplify)
+	test.EncodeAndRun(t, formula, context, filePath, id, expCode, simplify)
+	test.OnlyFeatVariables(t, context, "x")
+}
+
+func runGuardedConsConstVar(
+	t *testing.T,
+	id, expCode int,
+	c1, c2 base.Const,
+	simplify bool,
+) {
+	x := base.NewVar("x")
+	y := base.GuardedConst("y")
+	context := base.NewContext(DIM, nil)
+	context.Guards = append(
+		context.Guards,
+		base.Guard{Target: "y", Value: c1, Rep: "1"},
+	)
+	formula := operators.WithVar(
+		x,
+		operators.And(
+			operators.And(
+				subsumption.VarConst(x, c2),
+				subsumption.ConstVar(c2, x),
+			),
+			ConstVar(y, x),
+		),
+	)
+	filePath := test.CNFName(guardedConstVarSUFIX, id, simplify)
+	test.EncodeAndRun(t, formula, context, filePath, id, expCode, simplify)
+	test.OnlyFeatVariables(t, context, "x1", "y")
+}
+
+// =========================== //
+//            TESTS            //
+// =========================== //
+
+func TestConstVar_Encoding(t *testing.T) {
+	test.AddCleanup(t, constVarSUFIX, false)
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			runConsConstVar(t, i, tc.expCode, tc.val1, tc.val2, false)
+		})
+	}
+}
+
+func TestConstVar_Encoding_Guarded(t *testing.T) {
+	test.AddCleanup(t, guardedConstVarSUFIX, false)
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			runGuardedConsConstVar(t, i, tc.expCode, tc.val1, tc.val2, false)
+		})
+	}
+}
+
+func TestConstVar_Encoding_WrongDim(t *testing.T) {
+	x := base.Const{base.BOT, base.BOT, base.BOT}
+	y := base.NewVar("y")
+	formula := ConstVar(x, y)
+	context := base.NewContext(4, nil)
+	_, err := formula.Encoding(context)
+	if err == nil {
+		t.Error("Error not cached. Expected constant wrong dimension error")
+	}
+}
+
+func TestConstVar_Simplified(t *testing.T) {
+	test.AddCleanup(t, constVarSUFIX, true)
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			runConsConstVar(t, i, tc.expCode, tc.val1, tc.val2, true)
+		})
+	}
+}
+
+func TestConstVar_Simplified_Guarded(t *testing.T) {
+	test.AddCleanup(t, guardedConstVarSUFIX, true)
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			runGuardedConsConstVar(t, i, tc.expCode, tc.val1, tc.val2, true)
+		})
+	}
+}
+
+func TestConstVar_Simplified_WrongDim(t *testing.T) {
+	x := base.Const{base.BOT, base.BOT, base.BOT}
+	y := base.NewVar("y")
+	formula := ConstVar(x, y)
+	context := base.NewContext(4, nil)
+	_, err := formula.Simplified(context)
+	if err == nil {
+		t.Error("Error not cached. Expected constant wrong dimension error")
+	}
+}
+
+func TestConstVar_GetChildren(t *testing.T) {
+	x := base.Const{base.BOT, base.BOT, base.BOT}
+	y := base.NewVar("y")
+	formula := ConstVar(x, y)
+	children := formula.GetChildren()
+	if len(children) != 0 {
+		t.Errorf(
+			"Wrong number of children. Expected %d but got %d",
+			0,
+			len(children),
+		)
+	}
+}
+
+func TestConstVar_IsTrivial(t *testing.T) {
+	x := base.Const{base.BOT, base.BOT, base.BOT}
+	y := base.NewVar("y")
+	formula := ConstVar(x, y)
+	isTrivial, _ := formula.IsTrivial()
+	if isTrivial {
+		t.Errorf("Wrong IsTrivial value. Expected %t but got %t", false, true)
+	}
+}
+

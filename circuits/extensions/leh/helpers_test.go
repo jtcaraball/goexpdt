28dcedb75772175b8529fd7bedf7543181c91302
jo <jo@pop-os.localdomain>
2024-04-02 17:12:45 -0300

    feat: add leh test case.

--- circuits/extensions/leh/helpers_test.go
+++ circuits/extensions/leh/helpers_test.go
@@ -61,6 +61,13 @@ var tests = []struct {
 		expCode: 20,
 	},
 	{
+		name: "(1,0,0),(1,1,_),(1,1,0)",
+		val1: base.Const{base.ONE, base.ZERO, base.ZERO},
+		val2: base.Const{base.ONE, base.ONE, base.BOT},
+		val3: base.Const{base.ONE, base.ONE, base.ZERO},
+		expCode: 20,
+	},
+	{
 		name: "(1,1,1),(1,1,0),(1,_,0)",
 		val1: base.Const{base.ONE, base.ONE, base.ONE},
 		val2: base.Const{base.ONE, base.ONE, base.ZERO},

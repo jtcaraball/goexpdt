4805cea579809c2d06494714c3ce05134f8d6ae7
jo <jo@pop-os.localdomain>
2024-04-04 11:12:02 -0300

    feat: add neg tests to allcomp circuit.

--- circuits/extensions/allcomp/helpers_test.go
+++ circuits/extensions/allcomp/helpers_test.go
@@ -1,308 +1,582 @@
 package allcomp
 
 import (
-	"goexpdt/trees"
 	"goexpdt/base"
+	"goexpdt/trees"
 )
 
 const DIM = 3
 
-var allPosTests = []struct {
-	name string
-	val base.Const
+type testS struct {
+	name    string
+	val     base.Const
 	expCode int
-}{
+}
+
+var allPosTests = []testS{
 	{
-		name: "(_,_,_)",
-		val: base.Const{base.BOT, base.BOT, base.BOT},
+		name:    "(_,_,_)",
+		val:     base.Const{base.BOT, base.BOT, base.BOT},
 		expCode: 20,
 	},
 	{
-		name: "(0,_,_)",
-		val: base.Const{base.ZERO, base.BOT, base.BOT},
+		name:    "(0,_,_)",
+		val:     base.Const{base.ZERO, base.BOT, base.BOT},
 		expCode: 10,
 	},
 	{
-		name: "(1,_,_)",
-		val: base.Const{base.ONE, base.BOT, base.BOT},
+		name:    "(1,_,_)",
+		val:     base.Const{base.ONE, base.BOT, base.BOT},
 		expCode: 20,
 	},
 	{
-		name: "(0,0,_)",
-		val: base.Const{base.ZERO, base.ZERO, base.BOT},
+		name:    "(0,0,_)",
+		val:     base.Const{base.ZERO, base.ZERO, base.BOT},
 		expCode: 10,
 	},
 	{
-		name: "(0,1,_)",
-		val: base.Const{base.ZERO, base.ONE, base.BOT},
+		name:    "(0,1,_)",
+		val:     base.Const{base.ZERO, base.ONE, base.BOT},
 		expCode: 10,
 	},
 	{
-		name: "(1,0,_)",
-		val: base.Const{base.ONE, base.ZERO, base.BOT},
+		name:    "(1,0,_)",
+		val:     base.Const{base.ONE, base.ZERO, base.BOT},
 		expCode: 20,
 	},
 	{
-		name: "(1,1,_)",
-		val: base.Const{base.ONE, base.ONE, base.BOT},
+		name:    "(1,1,_)",
+		val:     base.Const{base.ONE, base.ONE, base.BOT},
 		expCode: 20,
 	},
 	{
-		name: "(0,0,0)",
-		val: base.Const{base.ZERO, base.ZERO, base.ZERO},
+		name:    "(0,0,0)",
+		val:     base.Const{base.ZERO, base.ZERO, base.ZERO},
 		expCode: 10,
 	},
 	{
-		name: "(0,0,1)",
-		val: base.Const{base.ZERO, base.ZERO, base.ONE},
+		name:    "(0,0,1)",
+		val:     base.Const{base.ZERO, base.ZERO, base.ONE},
 		expCode: 10,
 	},
 	{
-		name: "(0,1,0)",
-		val: base.Const{base.ZERO, base.ONE, base.ZERO},
+		name:    "(0,1,0)",
+		val:     base.Const{base.ZERO, base.ONE, base.ZERO},
 		expCode: 10,
 	},
 	{
-		name: "(0,1,1)",
-		val: base.Const{base.ZERO, base.ONE, base.ONE},
+		name:    "(0,1,1)",
+		val:     base.Const{base.ZERO, base.ONE, base.ONE},
 		expCode: 10,
 	},
 	{
-		name: "(1,0,0)",
-		val: base.Const{base.ONE, base.ZERO, base.ZERO},
+		name:    "(1,0,0)",
+		val:     base.Const{base.ONE, base.ZERO, base.ZERO},
 		expCode: 20,
 	},
 	{
-		name: "(1,0,1)",
-		val: base.Const{base.ONE, base.ZERO, base.ONE},
+		name:    "(1,0,1)",
+		val:     base.Const{base.ONE, base.ZERO, base.ONE},
 		expCode: 10,
 	},
 	{
-		name: "(1,1,0)",
-		val: base.Const{base.ONE, base.ONE, base.ZERO},
+		name:    "(1,1,0)",
+		val:     base.Const{base.ONE, base.ONE, base.ZERO},
 		expCode: 20,
 	},
 	{
-		name: "(1,1,1)",
-		val: base.Const{base.ONE, base.ONE, base.ONE},
+		name:    "(1,1,1)",
+		val:     base.Const{base.ONE, base.ONE, base.ONE},
 		expCode: 20,
 	},
 	{
-		name: "(_,0,_)",
-		val: base.Const{base.BOT, base.ZERO, base.BOT},
+		name:    "(_,0,_)",
+		val:     base.Const{base.BOT, base.ZERO, base.BOT},
 		expCode: 20,
 	},
 	{
-		name: "(_,1,_)",
-		val: base.Const{base.BOT, base.ONE, base.BOT},
+		name:    "(_,1,_)",
+		val:     base.Const{base.BOT, base.ONE, base.BOT},
 		expCode: 20,
 	},
 	{
-		name: "(_,0,0)",
-		val: base.Const{base.BOT, base.ZERO, base.ZERO},
+		name:    "(_,0,0)",
+		val:     base.Const{base.BOT, base.ZERO, base.ZERO},
 		expCode: 20,
 	},
 	{
-		name: "(_,0,1)",
-		val: base.Const{base.BOT, base.ZERO, base.ONE},
+		name:    "(_,0,1)",
+		val:     base.Const{base.BOT, base.ZERO, base.ONE},
 		expCode: 10,
 	},
 	{
-		name: "(_,1,0)",
-		val: base.Const{base.BOT, base.ONE, base.ZERO},
+		name:    "(_,1,0)",
+		val:     base.Const{base.BOT, base.ONE, base.ZERO},
 		expCode: 20,
 	},
 	{
-		name: "(_,1,1)",
-		val: base.Const{base.BOT, base.ONE, base.ONE},
+		name:    "(_,1,1)",
+		val:     base.Const{base.BOT, base.ONE, base.ONE},
 		expCode: 20,
 	},
 	{
-		name: "(_,_,0)",
-		val: base.Const{base.BOT, base.BOT, base.ZERO},
+		name:    "(_,_,0)",
+		val:     base.Const{base.BOT, base.BOT, base.ZERO},
 		expCode: 20,
 	},
 	{
-		name: "(_,_,1)",
-		val: base.Const{base.BOT, base.BOT, base.ONE},
+		name:    "(_,_,1)",
+		val:     base.Const{base.BOT, base.BOT, base.ONE},
 		expCode: 20,
 	},
 	{
-		name: "(0,_,0)",
-		val: base.Const{base.ZERO, base.BOT, base.ZERO},
+		name:    "(0,_,0)",
+		val:     base.Const{base.ZERO, base.BOT, base.ZERO},
 		expCode: 10,
 	},
 	{
-		name: "(0,_,1)",
-		val: base.Const{base.ZERO, base.BOT, base.ONE},
+		name:    "(0,_,1)",
+		val:     base.Const{base.ZERO, base.BOT, base.ONE},
 		expCode: 10,
 	},
 	{
-		name: "(1,_,0)",
-		val: base.Const{base.ONE, base.BOT, base.ZERO},
+		name:    "(1,_,0)",
+		val:     base.Const{base.ONE, base.BOT, base.ZERO},
 		expCode: 20,
 	},
 	{
-		name: "(1,_,1)",
-		val: base.Const{base.ONE, base.BOT, base.ONE},
+		name:    "(1,_,1)",
+		val:     base.Const{base.ONE, base.BOT, base.ONE},
 		expCode: 20,
 	},
 }
 
-var allNegTests = []struct{
-	name string
-	val base.Const
-	expCode int
-}{
+var allPosNotTests = []testS{
 	{
-		name: "(_,_,_)",
-		val: base.Const{base.BOT, base.BOT, base.BOT},
-		expCode: 20,
-	},
-	{
-		name: "(0,_,_)",
-		val: base.Const{base.ZERO, base.BOT, base.BOT},
-		expCode: 20,
-	},
-	{
-		name: "(1,_,_)",
-		val: base.Const{base.ONE, base.BOT, base.BOT},
-		expCode: 20,
-	},
-	{
-		name: "(0,0,_)",
-		val: base.Const{base.ZERO, base.ZERO, base.BOT},
-		expCode: 20,
-	},
-	{
-		name: "(0,1,_)",
-		val: base.Const{base.ZERO, base.ONE, base.BOT},
-		expCode: 20,
-	},
-	{
-		name: "(1,0,_)",
-		val: base.Const{base.ONE, base.ZERO, base.BOT},
-		expCode: 20,
-	},
-	{
-		name: "(1,1,_)",
-		val: base.Const{base.ONE, base.ONE, base.BOT},
+		name:    "(_,_,_)",
+		val:     base.Const{base.BOT, base.BOT, base.BOT},
 		expCode: 10,
 	},
 	{
-		name: "(0,0,0)",
-		val: base.Const{base.ZERO, base.ZERO, base.ZERO},
+		name:    "(0,_,_)",
+		val:     base.Const{base.ZERO, base.BOT, base.BOT},
 		expCode: 20,
 	},
 	{
-		name: "(0,0,1)",
-		val: base.Const{base.ZERO, base.ZERO, base.ONE},
-		expCode: 20,
-	},
-	{
-		name: "(0,1,0)",
-		val: base.Const{base.ZERO, base.ONE, base.ZERO},
-		expCode: 20,
-	},
-	{
-		name: "(0,1,1)",
-		val: base.Const{base.ZERO, base.ONE, base.ONE},
-		expCode: 20,
-	},
-	{
-		name: "(1,0,0)",
-		val: base.Const{base.ONE, base.ZERO, base.ZERO},
+		name:    "(1,_,_)",
+		val:     base.Const{base.ONE, base.BOT, base.BOT},
 		expCode: 10,
 	},
 	{
-		name: "(1,0,1)",
-		val: base.Const{base.ONE, base.ZERO, base.ONE},
+		name:    "(0,0,_)",
+		val:     base.Const{base.ZERO, base.ZERO, base.BOT},
 		expCode: 20,
 	},
 	{
-		name: "(1,1,0)",
-		val: base.Const{base.ONE, base.ONE, base.ZERO},
+		name:    "(0,1,_)",
+		val:     base.Const{base.ZERO, base.ONE, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(1,0,_)",
+		val:     base.Const{base.ONE, base.ZERO, base.BOT},
 		expCode: 10,
 	},
 	{
-		name: "(1,1,1)",
-		val: base.Const{base.ONE, base.ONE, base.ONE},
+		name:    "(1,1,_)",
+		val:     base.Const{base.ONE, base.ONE, base.BOT},
 		expCode: 10,
 	},
 	{
-		name: "(_,0,_)",
-		val: base.Const{base.BOT, base.ZERO, base.BOT},
+		name:    "(0,0,0)",
+		val:     base.Const{base.ZERO, base.ZERO, base.ZERO},
 		expCode: 20,
 	},
 	{
-		name: "(_,1,_)",
-		val: base.Const{base.BOT, base.ONE, base.BOT},
+		name:    "(0,0,1)",
+		val:     base.Const{base.ZERO, base.ZERO, base.ONE},
 		expCode: 20,
 	},
 	{
-		name: "(_,0,0)",
-		val: base.Const{base.BOT, base.ZERO, base.ZERO},
+		name:    "(0,1,0)",
+		val:     base.Const{base.ZERO, base.ONE, base.ZERO},
 		expCode: 20,
 	},
 	{
-		name: "(_,0,1)",
-		val: base.Const{base.BOT, base.ZERO, base.ONE},
+		name:    "(0,1,1)",
+		val:     base.Const{base.ZERO, base.ONE, base.ONE},
 		expCode: 20,
 	},
 	{
-		name: "(_,1,0)",
-		val: base.Const{base.BOT, base.ONE, base.ZERO},
-		expCode: 20,
-	},
-	{
-		name: "(_,1,1)",
-		val: base.Const{base.BOT, base.ONE, base.ONE},
-		expCode: 20,
-	},
-	{
-		name: "(_,_,0)",
-		val: base.Const{base.BOT, base.BOT, base.ZERO},
-		expCode: 20,
-	},
-	{
-		name: "(_,_,1)",
-		val: base.Const{base.BOT, base.BOT, base.ONE},
-		expCode: 20,
-	},
-	{
-		name: "(0,_,0)",
-		val: base.Const{base.ZERO, base.BOT, base.ZERO},
-		expCode: 20,
-	},
-	{
-		name: "(0,_,1)",
-		val: base.Const{base.ZERO, base.BOT, base.ONE},
-		expCode: 20,
-	},
-	{
-		name: "(1,_,0)",
-		val: base.Const{base.ONE, base.BOT, base.ZERO},
+		name:    "(1,0,0)",
+		val:     base.Const{base.ONE, base.ZERO, base.ZERO},
 		expCode: 10,
 	},
 	{
-		name: "(1,_,1)",
-		val: base.Const{base.ONE, base.BOT, base.ONE},
+		name:    "(1,0,1)",
+		val:     base.Const{base.ONE, base.ZERO, base.ONE},
 		expCode: 20,
+	},
+	{
+		name:    "(1,1,0)",
+		val:     base.Const{base.ONE, base.ONE, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(1,1,1)",
+		val:     base.Const{base.ONE, base.ONE, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(_,0,_)",
+		val:     base.Const{base.BOT, base.ZERO, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(_,1,_)",
+		val:     base.Const{base.BOT, base.ONE, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(_,0,0)",
+		val:     base.Const{base.BOT, base.ZERO, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(_,0,1)",
+		val:     base.Const{base.BOT, base.ZERO, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(_,1,0)",
+		val:     base.Const{base.BOT, base.ONE, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(_,1,1)",
+		val:     base.Const{base.BOT, base.ONE, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(_,_,0)",
+		val:     base.Const{base.BOT, base.BOT, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(_,_,1)",
+		val:     base.Const{base.BOT, base.BOT, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(0,_,0)",
+		val:     base.Const{base.ZERO, base.BOT, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(0,_,1)",
+		val:     base.Const{base.ZERO, base.BOT, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(1,_,0)",
+		val:     base.Const{base.ONE, base.BOT, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(1,_,1)",
+		val:     base.Const{base.ONE, base.BOT, base.ONE},
+		expCode: 10,
+	},
+}
+
+var allNegTests = []testS{
+	{
+		name:    "(_,_,_)",
+		val:     base.Const{base.BOT, base.BOT, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(0,_,_)",
+		val:     base.Const{base.ZERO, base.BOT, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(1,_,_)",
+		val:     base.Const{base.ONE, base.BOT, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(0,0,_)",
+		val:     base.Const{base.ZERO, base.ZERO, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(0,1,_)",
+		val:     base.Const{base.ZERO, base.ONE, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(1,0,_)",
+		val:     base.Const{base.ONE, base.ZERO, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(1,1,_)",
+		val:     base.Const{base.ONE, base.ONE, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(0,0,0)",
+		val:     base.Const{base.ZERO, base.ZERO, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(0,0,1)",
+		val:     base.Const{base.ZERO, base.ZERO, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(0,1,0)",
+		val:     base.Const{base.ZERO, base.ONE, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(0,1,1)",
+		val:     base.Const{base.ZERO, base.ONE, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(1,0,0)",
+		val:     base.Const{base.ONE, base.ZERO, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(1,0,1)",
+		val:     base.Const{base.ONE, base.ZERO, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(1,1,0)",
+		val:     base.Const{base.ONE, base.ONE, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(1,1,1)",
+		val:     base.Const{base.ONE, base.ONE, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(_,0,_)",
+		val:     base.Const{base.BOT, base.ZERO, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(_,1,_)",
+		val:     base.Const{base.BOT, base.ONE, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(_,0,0)",
+		val:     base.Const{base.BOT, base.ZERO, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(_,0,1)",
+		val:     base.Const{base.BOT, base.ZERO, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(_,1,0)",
+		val:     base.Const{base.BOT, base.ONE, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(_,1,1)",
+		val:     base.Const{base.BOT, base.ONE, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(_,_,0)",
+		val:     base.Const{base.BOT, base.BOT, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(_,_,1)",
+		val:     base.Const{base.BOT, base.BOT, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(0,_,0)",
+		val:     base.Const{base.ZERO, base.BOT, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(0,_,1)",
+		val:     base.Const{base.ZERO, base.BOT, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(1,_,0)",
+		val:     base.Const{base.ONE, base.BOT, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(1,_,1)",
+		val:     base.Const{base.ONE, base.BOT, base.ONE},
+		expCode: 20,
+	},
+}
+
+var allNegNotTests = []testS{
+	{
+		name:    "(_,_,_)",
+		val:     base.Const{base.BOT, base.BOT, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(0,_,_)",
+		val:     base.Const{base.ZERO, base.BOT, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(1,_,_)",
+		val:     base.Const{base.ONE, base.BOT, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(0,0,_)",
+		val:     base.Const{base.ZERO, base.ZERO, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(0,1,_)",
+		val:     base.Const{base.ZERO, base.ONE, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(1,0,_)",
+		val:     base.Const{base.ONE, base.ZERO, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(1,1,_)",
+		val:     base.Const{base.ONE, base.ONE, base.BOT},
+		expCode: 20,
+	},
+	{
+		name:    "(0,0,0)",
+		val:     base.Const{base.ZERO, base.ZERO, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(0,0,1)",
+		val:     base.Const{base.ZERO, base.ZERO, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(0,1,0)",
+		val:     base.Const{base.ZERO, base.ONE, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(0,1,1)",
+		val:     base.Const{base.ZERO, base.ONE, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(1,0,0)",
+		val:     base.Const{base.ONE, base.ZERO, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(1,0,1)",
+		val:     base.Const{base.ONE, base.ZERO, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(1,1,0)",
+		val:     base.Const{base.ONE, base.ONE, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(1,1,1)",
+		val:     base.Const{base.ONE, base.ONE, base.ONE},
+		expCode: 20,
+	},
+	{
+		name:    "(_,0,_)",
+		val:     base.Const{base.BOT, base.ZERO, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(_,1,_)",
+		val:     base.Const{base.BOT, base.ONE, base.BOT},
+		expCode: 10,
+	},
+	{
+		name:    "(_,0,0)",
+		val:     base.Const{base.BOT, base.ZERO, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(_,0,1)",
+		val:     base.Const{base.BOT, base.ZERO, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(_,1,0)",
+		val:     base.Const{base.BOT, base.ONE, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(_,1,1)",
+		val:     base.Const{base.BOT, base.ONE, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(_,_,0)",
+		val:     base.Const{base.BOT, base.BOT, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(_,_,1)",
+		val:     base.Const{base.BOT, base.BOT, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(0,_,0)",
+		val:     base.Const{base.ZERO, base.BOT, base.ZERO},
+		expCode: 10,
+	},
+	{
+		name:    "(0,_,1)",
+		val:     base.Const{base.ZERO, base.BOT, base.ONE},
+		expCode: 10,
+	},
+	{
+		name:    "(1,_,0)",
+		val:     base.Const{base.ONE, base.BOT, base.ZERO},
+		expCode: 20,
+	},
+	{
+		name:    "(1,_,1)",
+		val:     base.Const{base.ONE, base.BOT, base.ONE},
+		expCode: 10,
 	},
 }
 
 func genTree() *trees.Tree {
-    // root: _,_,_
-    //   node_1: 0,_,_
-    //       node_3: 0,0,_
-    //           leaf_1: 0,0,0 (True)
-    //           leaf_2: 0,0,1 (True)
-    //       leaf_3: 0,1,_ (True)
-    //   node_2: 1,_,_
-    //       leaf_4: 1,_,0 (False)
-    //       node_4: 1,_,1
-    //           leaf_5: 1,0,1 (True)
-    //           leaf_6: 1,1,1 (False)
+	// root: _,_,_
+	//   node_1: 0,_,_
+	//       node_3: 0,0,_
+	//           leaf_1: 0,0,0 (True)
+	//           leaf_2: 0,0,1 (True)
+	//       leaf_3: 0,1,_ (True)
+	//   node_2: 1,_,_
+	//       leaf_4: 1,_,0 (False)
+	//       node_4: 1,_,1
+	//           leaf_5: 1,0,1 (True)
+	//           leaf_6: 1,1,1 (False)
 	leaf1 := &trees.Node{ID: 5, Value: true}
 	leaf2 := &trees.Node{ID: 6, Value: true}
 	leaf3 := &trees.Node{ID: 7, Value: true}
@@ -315,10 +589,10 @@ func genTree() *trees.Tree {
 	node1 := &trees.Node{ID: 1, Feat: 1, LChild: node3, RChild: leaf3}
 	root := &trees.Node{ID: 0, Feat: 0, LChild: node1, RChild: node2}
 	return &trees.Tree{
-		Root: root,
+		Root:      root,
 		NodeCount: 11,
 		FeatCount: 3,
-		PosLeafs: []*trees.Node{leaf1, leaf2, leaf3, leaf5},
-		NegLeafs: []*trees.Node{leaf4, leaf6},
+		PosLeafs:  []*trees.Node{leaf1, leaf2, leaf3, leaf5},
+		NegLeafs:  []*trees.Node{leaf4, leaf6},
 	}
 }

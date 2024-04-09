c2a3d6b4f511ee2644620e1b32e68d0eff516d30
jo <jo@pop-os.localdomain>
2024-04-08 20:45:27 -0400

    feat: add context reset method.

--- base/context.go
+++ base/context.go
@@ -1,8 +1,8 @@
 package base
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 	"goexpdt/trees"
 	"slices"
 	"strconv"
@@ -13,27 +13,27 @@ import (
 // =========================== //
 
 type Context struct {
-	Dimension int
-	Tree *trees.Tree
-	TopV int
-	Guards []Guard
-	nodeConsts []Const
+	Dimension     int
+	Tree          *trees.Tree
+	TopV          int
+	Guards        []Guard
+	nodeConsts    []Const
 	posLeafConsts []Const
 	negLeafConsts []Const
-	featVars map[ContextVar]int
-	interVars map[ContextVar]int
+	featVars      map[ContextVar]int
+	interVars     map[ContextVar]int
 }
 
 type Guard struct {
-	Target string
+	Target  string
 	InScope []string
-	Value Const
-	Idx int
+	Value   Const
+	Idx     int
 }
 
 type ContextVar struct {
-	Name string
-	Idx int
+	Name  string
+	Idx   int
 	Value int
 }
 
@@ -49,8 +49,16 @@ func NewContext(dim int, tree *trees.Tree) *Context {
 	return ctx
 }
 
+// Reset conext variables and guards.
+func (c *Context) Reset() {
+	c.TopV = 0
+	c.featVars = make(map[ContextVar]int)
+	c.interVars = make(map[ContextVar]int)
+	c.Guards = make([]Guard, 0)
+}
+
 // Return assigned value to variable. If it does not exist it is added.
-func (c* Context) Var(name string, idx int, value int) int {
+func (c *Context) Var(name string, idx int, value int) int {
 	varS := ContextVar{Name: name, Idx: idx, Value: value}
 	varValue := c.featVars[varS]
 	if varValue == 0 {
@@ -68,7 +76,7 @@ func (c *Context) VarExists(name string, idx int, value int) bool {
 }
 
 // Return assigned value to internal variable. If it does not exist it is added.
-func (c* Context) IVar(name string, idx int, value int) int {
+func (c *Context) IVar(name string, idx int, value int) int {
 	varS := ContextVar{Name: name, Idx: idx, Value: value}
 	varValue := c.interVars[varS]
 	if varValue == 0 {
@@ -87,7 +95,7 @@ func (c *Context) IVarExists(name string, idx int, value int) bool {
 
 // Set context's TopV to the max between the current value and value passed.
 // Returns true if the value was updated.
-func (c* Context) MaxUpdateTopV(topv int) bool {
+func (c *Context) MaxUpdateTopV(topv int) bool {
 	if topv > c.TopV {
 		c.TopV = topv
 		return true
@@ -159,7 +167,7 @@ func (c *Context) GuardValueByTarget(target string) (Const, error) {
 
 // Node iteration struct.
 type nodeElem struct {
-	Node *trees.Node
+	Node  *trees.Node
 	Value Const
 }
 
@@ -170,13 +178,13 @@ func (c *Context) NodesAsConsts() ([]Const, error) {
 	}
 	var node *trees.Node
 	var nConst, lnConst, rnConst Const
-	var nConsts = []Const{}
 	var nInfo nodeElem
-	var nStack = []nodeElem{
+	nConsts := []Const{}
+	nStack := []nodeElem{
 		{Node: c.Tree.Root, Value: AllBotConst(c.Dimension)},
 	}
 	for len(nStack) > 0 {
-		nInfo, nStack = nStack[len(nStack) - 1], nStack[:len(nStack) - 1]
+		nInfo, nStack = nStack[len(nStack)-1], nStack[:len(nStack)-1]
 		node, nConst = nInfo.Node, nInfo.Value
 		// Check for valid indexing.
 		if node.Feat >= c.Dimension || node.Feat < 0 {
@@ -213,11 +221,11 @@ func (c *Context) LeafsAsConsts() ([]Const, []Const, error) {
 	var nConst, lnConst, rnConst Const
 	var pnConsts, nnConsts []Const
 	var nInfo nodeElem
-	var nStack = []nodeElem{
+	nStack := []nodeElem{
 		{Node: c.Tree.Root, Value: AllBotConst(c.Dimension)},
 	}
 	for len(nStack) > 0 {
-		nInfo, nStack = nStack[len(nStack) - 1], nStack[:len(nStack) - 1]
+		nInfo, nStack = nStack[len(nStack)-1], nStack[:len(nStack)-1]
 		node, nConst = nInfo.Node, nInfo.Value
 		// Check for valid indexing.
 		if node.Feat >= c.Dimension || node.Feat < 0 {

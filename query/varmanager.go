5b7bdf24c19f1b542e10479f9132e31bd1efd42e
jo <jo@pop-os.localdomain>
2024-06-01 18:16:00 -0400

    refact: remove IVar related methods in varmanager.

--- query/varmanager.go
+++ query/varmanager.go
@@ -1,10 +1,9 @@
 package query
 
 // VarManager manages the creation and usage of cnf variables. Variables are
-// divided into user and internal variables, are denoted by the triple (name,
-// idx, val) and are assigned positive integer values. All variables must be
-// assigned a value equal or greater than 1 as 0 is reserved by the DIMACS
-// standard.
+// denoted by the triple (name, idx, val) and are assigned positive integer
+// values. All variables must be assigned a value equal or greater than 1 as 0
+// is reserved by the DIMACS standard.
 type VarManager interface {
 	// TopV returns the value of largest unsigned integer assigned to a
 	// variable. This value is expected to be always positive.
@@ -13,27 +12,20 @@ type VarManager interface {
 	// integer to be assigned. Returns true if the update is valid and took
 	// effect.
 	UpdateTopV(v int) bool
-	// Var return the unsigned integer value assigned to the user variable
-	// given. If the variable does not exists it first assigns a value to it.
-	// This value is expected to be always positive.
+	// Var return the unsigned integer value assigned to the variable given. If
+	// the variable does not exists it first assigns a value to it. This value
+	// is expected to be always positive.
 	Var(name string, idx, val int) int
-	// VarExists return true if the user variable given exists.
+	// VarExists return true if the passed variable exists.
 	VarExists(name string, idx, val int) bool
-	// Var return the unsigned integer value assigned to the internal variable
-	// given. If the variable does not exists it first assigns a value to it.
-	// This value is expected to be always positive.
-	IVar(name string, idx, val int) int
-	// VarExists return true if the internal variable given exists.
-	IVarExists(name string, idx, val int) bool
 	// Reset removes all variables and sets the next variable to be assigned
-	// to 0.
+	// to 1.
 	Reset()
 }
 
 type baseVarManager struct {
 	topv         int            // Value of the largest variable assigned.
-	userVars     map[varRep]int // User variables map.
-	internalVars map[varRep]int // Internal variables map.
+	vars     map[varRep]int // User variables map.
 }
 
 type varRep struct {
@@ -56,36 +48,20 @@ func (vm *baseVarManager) UpdateTopV(v int) bool {
 
 func (vm *baseVarManager) Var(name string, idx, value int) int {
 	v := varRep{name, idx, value}
-	if val, ok := vm.userVars[v]; ok {
+	if val, ok := vm.vars[v]; ok {
 		return val
 	}
 	vm.topv += 1
-	vm.userVars[v] = vm.topv
+	vm.vars[v] = vm.topv
 	return vm.topv
 }
 
 func (vm *baseVarManager) VarExists(name string, idx, value int) bool {
-	_, ok := vm.userVars[varRep{name, idx, value}]
-	return ok
-}
-
-func (vm *baseVarManager) IVar(name string, idx, value int) int {
-	v := varRep{name, idx, value}
-	if val, ok := vm.internalVars[v]; ok {
-		return val
-	}
-	vm.topv += 1
-	vm.internalVars[v] = vm.topv
-	return vm.topv
-}
-
-func (vm *baseVarManager) IVarExists(name string, idx, value int) bool {
-	_, ok := vm.internalVars[varRep{name, idx, value}]
+	_, ok := vm.vars[varRep{name, idx, value}]
 	return ok
 }
 
 func (vm *baseVarManager) Reset() {
 	vm.topv = 0
-	vm.userVars = nil
-	vm.internalVars = nil
+	vm.vars = nil
 }

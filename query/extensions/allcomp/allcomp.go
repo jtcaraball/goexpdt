fad6d1f8793c04fc7b3a3a54324c0701764ddda5
jo <jo@pop-os.localdomain>
2024-07-17 19:30:19 -0400

    feat: add documentation for every query extension.

---
+++ query/extensions/allcomp/allcomp.go
@@ -0,0 +1,6 @@
+// AllPos and AllNeg formulas in the DT-FOIL logic.
+//
+// Given a model T and partial instance e and a truth value c we say that T
+// satisfies AllComp(e, c) if and only if all completions e' of e receive the
+// classification c over T.
+package allcomp

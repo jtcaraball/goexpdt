62fb6276527aed9eeb3329666f5802a3b461656d
Jose Caraball <jtcaraball@Joses-Air.lan>
2024-08-25 19:58:54 -0400

    feat: add compute pkg short description.

---
+++ compute/compute.go
@@ -0,0 +1,7 @@
+// The compute package allows for the computation Opt-DT-Foil through the
+// ComputeOptim function and evaluation of Q-DT-Foil formulas through the
+// various QDTFCombinator interface implementations.
+//
+// The Solver interface is defined in order to interact with SAT solvers using
+// the DIMACS CNF standard.
+package compute
